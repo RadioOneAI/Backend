1580a84d8d36d82df6651e07a1e6be3654218295
YasiruNB <155614875+ynbdissanayake@users.noreply.github.com>
2025-12-28 16:50:45 +0530

    Setup User model

---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from .user import User, Role, AccountStatus, LICENSE_REQUIRED_ROLES
+from .prescription import Prescription, PrescriptionImage, PrescriptionStatus
+from .report import Report, ReportImage, ReportStatus
+from .feedback import ReportFeedback
+from .notice import Notice, NoticePriority
