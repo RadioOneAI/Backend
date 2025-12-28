5bc613fc10322c2b0aead2cf8a17cc9d2116b370
YasiruNB <155614875+ynbdissanayake@users.noreply.github.com>
2025-12-28 13:49:47 +0530

    Setup initial Billing Service structure with integration

--- app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .user import User, Role, AccountStatus, LICENSE_REQUIRED_ROLES
-from .prescription import Prescription, PrescriptionImage, PrescriptionStatus
-from .report import Report, ReportImage, ReportStatus
-from .feedback import ReportFeedback
-from .notice import Notice, NoticePriority
