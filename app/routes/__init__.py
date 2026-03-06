3fc016525b07d16e3b4a79dfef00a1037b604d5d
YasiruNB <155614875+ynbdissanayake@users.noreply.github.com>
2026-03-06 14:56:04 +0530

    Prescription routes updated.

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,7 +3,6 @@ from .admin_routes import admin_bp
 from .profile_routes import profile_bp
 from .patient_routes import patients_bp
 from .prescriptions import prescriptions_bp
-from .patient_prescriptions import patient_prescriptions_bp
 from app.routes.scan_images import scan_images_bp
 
 def register_routes(app):
@@ -12,5 +11,4 @@ def register_routes(app):
     app.register_blueprint(profile_bp)
     app.register_blueprint(patients_bp)
     app.register_blueprint(prescriptions_bp)
-    app.register_blueprint(patient_prescriptions_bp)
     app.register_blueprint(scan_images_bp)
